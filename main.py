d596a8bfea80e2112b0bfa4f31f525efd0de42d6
ReaganBruce <reaganbrucebirmingham@gmail.com>
2023-01-21 13:36:19 -0600

    added conditionals; more sys output in command line

--- main.py
+++ main.py
@@ -5,24 +5,32 @@ import datetime
 import os
 
 file_path = os.getenv('FILE_PATH') #replace file path with your own
+ls_dir = os.listdir(file_path) #list items in directory
 
 def download(link):
     youtube_video = YouTube(link)
     youtube_length = str(datetime.timedelta(seconds=youtube_video.length)) #convert seconds to hours/minutes/seconds
     
     print('-' * 50)
-    print('VIDEO INFORMATION:')
+    print('Video Information:'.upper())
     print('Author: {0}'.format(youtube_video.author))
     print('Title: {0}'.format(youtube_video.title))  
     print('Length: {0}'.format(youtube_length))   
     print('Views: {0}'.format(youtube_video.views))   
     print('-' * 50)
     
-    to_download = input('Continue downloading this video? \n(y/n) ').casefold()
+    if len(ls_dir) != 0:
+        print('Current files in directory:'.upper())
+        for index, ls in enumerate(ls_dir):
+            print('{0}. {1}'.format(index + 1, ls)) #enumerate over items in current directory and prints result at index 1
+    else:
+        print('Nothing found in directory: {0}'.format(file_path).upper())
+    
+    to_download = input('\nContinue downloading this video? \n(y/n) ').casefold() #second input
     
     if to_download == 'y':
         try:
-            print('Downloading...........')
+            print('Downloading...........\n')
             youtube_video.streams.get_highest_resolution().download(file_path)
             print('Download completed at file location: {0}'.format(file_path))
         except Exception as err:
