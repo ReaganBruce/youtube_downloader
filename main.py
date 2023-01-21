218b3c6ab540994c3c63a7696bb86e4137c1ce82
ReaganBruce <reaganbrucebirmingham@gmail.com>
2023-01-20 19:32:01 -0600

    fixed variable names; added path string formatting when video has been downloaded

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ import os
 file_path = os.getenv('FILE_PATH') #replace file path with your own
 
 def download(link):
-    youtubeVideo = YouTube(link)
-    youtubeLength = str(datetime.timedelta(seconds=youtubeVideo.length)) #convert seconds to hours/minutes/seconds
+    youtube_video = YouTube(link)
+    youtube_length = str(datetime.timedelta(seconds=youtube_video.length)) #convert seconds to hours/minutes/seconds
     
     print('-' * 50)
     print('VIDEO INFORMATION:')
-    print('Author: {}'.format(youtubeVideo.author))
-    print('Title: {}'.format(youtubeVideo.title))  
-    print('Length: {}'.format(youtubeLength))   
-    print('Views: {}'.format(youtubeVideo.views))   
+    print('Author: {}'.format(youtube_video.author))
+    print('Title: {}'.format(youtube_video.title))  
+    print('Length: {}'.format(youtube_length))   
+    print('Views: {}'.format(youtube_video.views))   
     print('-' * 50)
     
     to_download = input('Continue downloading this video? \n(y/n) ').casefold()
@@ -23,7 +23,7 @@ def download(link):
     if to_download == 'y':
         try:
             print('Downloading...........')
-            youtubeVideo.streams.get_highest_resolution().download(file_path)
+            youtube_video.streams.get_highest_resolution().download(file_path)
             print('Download completed!')
         except Exception as err:
             print(err)
